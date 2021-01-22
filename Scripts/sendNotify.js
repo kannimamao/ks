5c4f1a2d492f4346c3101265d0f3572901bba161
Yoga <50315632+yogayyy@users.noreply.github.com>
2021-01-22 22:11:47 +0800

    Update sendNotify.js

--- Scripts/sendNotify.js
+++ Scripts/sendNotify.js
@@ -127,7 +127,7 @@ if (process.env.PUSH_PLUS_USER) {
 
 async function sendNotify(text, desp, params = {}) {
   //提供7种通知
-  desp += `\n本脚本开源免费使用 By：https://github.com/LXK9301/jd_scripts`;
+  desp += ``;
   await Promise.all([
     serverNotify(text, desp),//微信server酱
     pushPlusNotify(text, desp)//pushplus(推送加)
