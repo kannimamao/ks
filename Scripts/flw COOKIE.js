687342a370836614269414c6d0d4ac7fe125937c
Yoga <50315632+yogayyy@users.noreply.github.com>
2021-01-24 21:50:09 +0800

    Update flw COOKIE.js

--- Scripts/flw COOKIE.js
+++ Scripts/flw COOKIE.js
@@ -1,19 +1,14 @@
-
-
-//独立COOKIE文件     ck在``里面填写，多账号换行
+//独立COOKIE文件，ck在``里面填写，多账号换行
 let flwurlVal= ``
 let flwheaderVal= ``
 let flwspbodyVal= ``
 let flwqwbodyVal= ``
-
-
 
 let flwcookie = {
   flwurlVal: flwurlVal,  
   flwheaderVal: flwheaderVal,  
   flwspbodyVal: flwspbodyVal, 
   flwqwbodyVal: flwqwbodyVal, 
-
 
 }
 
